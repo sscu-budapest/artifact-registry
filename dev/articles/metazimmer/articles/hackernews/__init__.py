78fad27528b15ef1f6017a4e1595f1cb2d5200e5
Endre Márk Borza <endremborza@gmail.com>
2022-03-24 22:47:54 +0100

    build-articles-0.0

---
+++ dev/articles/metazimmer/articles/hackernews/__init__.py
@@ -0,0 +1,22 @@
+from datetime import datetime  # noqa: F401
+
+import datazimmer as dz
+
+
+class Post(dz.BaseEntity):
+    pass
+
+
+class PostFeatures(dz.TableFeaturesBase):
+    post_id = str
+    rank = int
+    title = str
+    link = str
+    sitebit = str
+    posted = str
+    score = int
+    poster = str
+    comments = int
+
+
+post_table = dz.ScruTable(features=PostFeatures, subject_of_records=Post, max_partition_size=10000)
