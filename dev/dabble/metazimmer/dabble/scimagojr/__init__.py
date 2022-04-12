b0c5d832650ebf82d9405369664b62a50be3d4cc
Endre Márk Borza <endremborza@gmail.com>
2022-04-12 21:12:24 +0200

    build-dabble-0.1

---
+++ dev/dabble/metazimmer/dabble/scimagojr/__init__.py
@@ -0,0 +1,33 @@
+from datetime import datetime  # noqa: F401
+
+import datazimmer as dz
+
+
+class Journal(dz.BaseEntity):
+    pass
+
+
+class JournalFeatures(dz.TableFeaturesBase):
+    rank = int
+    title = str
+    type = str
+    issn = str
+    h_index = int
+    total_docs_2020 = int
+    ref_per_doc = float
+    sjr_best_quartile = str
+    total_docs_3years = int
+    total_refs = int
+    total_cites_3years = int
+    citable_docs_3years = int
+    country = str
+    region = str
+    publisher = str
+    coverage = str
+
+
+class JournalIndex(dz.IndexBase):
+    sourceid = int
+
+
+journal_table = dz.ScruTable(features=JournalFeatures, subject_of_records=Journal, index=JournalIndex)
